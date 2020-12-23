dbd58c2cf1f873445fe954c17f67f8b60c9065bb
ryo-asari <3omiryo@gmail.com>
2020-12-23 13:55:46 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,4 +9,14 @@ class Item < ApplicationRecord
   belongs_to :shipping
   belongs_to :prefecture
   belongs_to :pday
+
+  validates :title, :text, presence: true
+  validates :explanation, text, presence: true
+  validates :category_id, numericality: { other_than: 1 }
+  validates :condition_id, numericality: { other_than: 1 }
+  validates :shipping_id, numericality: { other_than: 1 }
+  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :pday_id, numericality: { other_than: 1 }
+  validates :price, numericality
+
 end
