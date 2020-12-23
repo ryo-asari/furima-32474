c44f0793a74dd765a1eb21cc3849532cd3d61359
ryo-asari <3omiryo@gmail.com>
2020-12-23 16:25:59 +0900

    出品商品の保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,22 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
 
   def new
-    
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:title, :explanation, :category_id, :condition_id, :shipping_id, :prefecture_id, :pday_id, :price, :image).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -10,14 +10,14 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :pday
 
-  validates :title, :text, presence: true
-  validates :explanation, text, presence: true
+  validates :title, presence: true
+  validates :explanation, presence: true
   validates :category_id, numericality: { other_than: 1 }
   validates :condition_id, numericality: { other_than: 1 }
   validates :shipping_id, numericality: { other_than: 1 }
-  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :prefecture_id, numericality: { other_than: 0 }
   validates :pday_id, numericality: { other_than: 1 }
-  validates :price, numericality
+  validates :price, presence: true
   validates :image, presence: true
 
 end
